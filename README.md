e3a91b9c970ba9af8c896642659abe80606938fa
psychobunny <psycho.bunny@hotmail.com>
2013-10-13 19:18:52 -0400

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NodeBB Cash MOD
+
+This plugin creates a virtual currency system for your NodeBB forum. Users can then earn cash based on the quality of their posts (ie. bigger posts earn more points).
+
+The amount earned per character can be defined in the Cash ACP, as well as the name of the currency itself.
+
+Top earners can be seen at /users/cash.
+
+## Installation
+
+    npm install nodebb-plugin-cash
+
+## Screenshots
+
+![NodeBB Cash MOD - Topics page]()
+![NodeBB Cash MOD - Admin Page]()
+![NodeBB Cash MOD - Top earners page]()
+
+
+## Credits
+
+Coin Art by [morgan3d](http://opengameart.org/users/morgan3d).
---
+++ library.js
@@ -0,0 +1,84 @@
+var	fs = require('fs'),
+	path = require('path'),
+	nconf = require('nconf'),
+	meta = require('../../src/meta.js'),
+	user = require('../../src/user.js'),
+	templates = module.parent.require('../public/src/templates.js');
+	
+var constants = Object.freeze({
+	'name': "Cash MOD",
+	'admin': {
+		'route': '/cash',
+		'icon': 'icon-money'
+	},
+	'defaults': {
+		'pay_per_character': 0.25,
+		'currency_name': 'gp'
+	}
+});
+
+
+CashMOD = {
+	// TODO: Move these into an "admin" class, I cannot do this until plugins.js is updated to register hooks as such 
+	admin_registerPlugin: function(custom_header, callback) {
+		custom_header.plugins.push({
+			"route": constants.admin.route,
+			"icon": constants.admin.icon,
+			"name": constants.name
+		});
+
+		return custom_header;
+	},
+	admin_addRoute: function(custom_routes, callback) {
+		fs.readFile(path.resolve(__dirname, './static/admin.tpl'), function (err, template) {
+			custom_routes.routes.push({
+				"route": constants.admin.route,
+				"method": "get",
+				"options": function(req, res, callback) {
+					callback({
+						req: req,
+						res: res,
+						route: constants.admin.route,
+						name: constants.name,
+						content: template
+					});
+				}
+			});
+
+			callback(null, custom_routes);
+		});
+	},
+	
+
+	addProfileInfo: function(profileInfo, callback) {
+		var currency_name = meta.config['cash:currency_name'] || constants.defaults.constants.defaults.currency_name;
+
+		user.getUserField(profileInfo.uid, 'currency', function(err, data) {
+			profileInfo.profile += "<span class='cash-mod-currency'><img src='" + nconf.get('url') + "plugins/nodebb-plugin-cash/coin1.png' /> " + (data || 0) + " " + currency_name + "</span>";
+			callback(err, profileInfo);
+		});		
+	},
+
+	increaseCurrencyByPostData: function(postData) {
+		var multiplier = meta.config['cash:pay_per_character'] || constants.defaults.pay_per_character,
+			uid = postData.uid,
+			postLength = postData.content.length,
+			value = Math.floor(multiplier * postLength);
+
+		user.incrementUserFieldBy(uid, 'currency', value);
+
+		setTimeout(function() {
+			global.io.sockets.in('uid_' + uid).emit('event:alert', {
+				alert_id: 'currency_increased',
+				message: 'You earned <strong>' + value + ' gold</strong> for posting',
+				type: 'info',
+				timeout: 2500
+			});
+		}, 750);
+		
+	}
+
+
+};
+
+module.exports = CashMOD;
